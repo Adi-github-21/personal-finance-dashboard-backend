54b6a10ac2bece3923babf343c4362791ac8466a
Adi-github-21 <adityasinghson3@gmail.com>
2025-07-22 10:50:48 +0530

    removed some comments

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const dotenv = require('dotenv'); // .env files load karne ke liye
-const connectDB = require('./config/db'); // Database connection function import kiya
+const dotenv = require('dotenv'); 
+const connectDB = require('./config/db'); 
 const authRoutes = require('./routes/auth'); // Authentication routes import kiye
 const bankAccountRoutes = require('./routes/bankAccounts');
 const investmentRoutes = require('./routes/investments');
-const cors = require('cors'); // CORS middleware import kiya
+const cors = require('cors'); // CORS middleware 
 const path = require('path'); 
 
-// .env file se environment variables load karo
 dotenv.config();
 
-// Database se connect karo
 connectDB();
 
-const app = express(); // Express app banayi
+const app = express(); 
 
 // Middleware
 app.use(express.json()); // Body parser middleware: JSON request body ko parse karne ke liye
@@ -32,7 +30,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-const PORT = process.env.PORT || 5000; // Port number .env se ya default 5000
+const PORT = process.env.PORT || 5000;
 
 // Server start karo
 app.listen(PORT, () => {
